0e125efab9d8baaed0845bb1747e7c0a111404ad
k-humphrey <humphreykayla905@gmail.com>
2025-11-02 15:42:07 -0600

    scaffolded statsconnector files

---
+++ statisticsConnector.cpp
@@ -0,0 +1,18 @@
+/* Kayla Humphrey
+   Pseudo-code from Dr. Mike Rogers
+   Statistics Connector, generates statistics and sends them on to Queue
+   11/2/2025
+*/
+#include "statisticsConnector.hpp"
+
+int main(){
+
+}
+
+void statistics_connector(){
+    
+}
+
+int adapter_vmstat_to_message(std::string line, std::string converted){
+
+}
---
+++ statisticsConnector.hpp
@@ -0,0 +1,29 @@
+/*
+    Kayla Humphrey
+    Header file for statisticsConnector
+    11/2/2025
+*/
+#ifndef STATISTICSCONNECTOR_HPP
+#define STATISTICSCONNECTOR_HPP
+    #include <iostream>
+    #include <string>
+    #include <boost/process.hpp>
+    namespace bp = boost::process;
+
+    /*
+        func: statistics_connector
+        parameters: ?
+        return: ?
+        purpose: ?
+    */
+   void statistics_connector();
+
+   /*
+        func: adapter_vmstat_to_message()
+        parameters: LINE- a string of the line to be parsed and wrapped into an appropriate message for Queue
+                    CONVERTED - a string that will contain the converted line
+        return: an int that describes the length of CONVERTED
+        purpose: ?
+   */
+  int adapter_vmstat_to_message(std::string, std::string);
+#endif
