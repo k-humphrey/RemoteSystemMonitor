f7e2991cf693d54752cd55600aecee6828b6235d
k-humphrey <humphreykayla905@gmail.com>
2025-11-04 19:58:06 -0600

    Added Queue forwarder

---
+++ Queue.cpp
@@ -0,0 +1,26 @@
+/*
+    Kayla Humphrey
+    Message Queue
+    11/4/2025
+*/
+#include <zmq.hpp>
+
+int main(){
+    //create context
+    zmq::context_t ctx(1);  
+
+    //create xub and xsub sockets
+    zmq::socket_t xpubSocket(ctx, zmq::socket_type::xpub);
+    zmq::socket_t xsubSocket(ctx, zmq::socket_type::xsub);
+
+    //PUBLISHERS : 5555, SUBSCRIBERS : 5556
+    xpubSocket.bind("tcp://*:5555");
+    xsubSocket.bind("tcp://*:5556");
+
+    zmq::proxy(xsubSocket, xpubSocket);
+
+    xpubSocket.close();
+    xsubSocket.close();
+    ctx.close();
+    return 0;
+}
